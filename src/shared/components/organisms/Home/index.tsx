8d85d1d7c56179c6ba6c4feb2647677128dfca32
yukishinonome <yuki.yamakoshi0126@gmail.com>
2019-03-04 13:43:05 +0900

    add handlePreparedCheck

--- src/shared/components/organisms/Home/index.tsx
+++ src/shared/components/organisms/Home/index.tsx
@@ -1,15 +1,18 @@
 import { connect } from 'react-redux'
 import Home from './Home'
-import { currentTimeUpdate } from '../../../redux/modules/home'
+import { currentTimeUpdate, preparedCheck } from '../../../redux/modules/home'
 import { homeSelector } from '../../../redux/modules/reducer';
 
 export default connect(
   (state) => ({
+    count: homeSelector(state as any).count,
     today: homeSelector(state as any).today,
     time: homeSelector(state as any).time,
     itemList: homeSelector(state as any).itemList,
   }),
   (dispatch) => ({
     handleCurrentTimeUpdate: () => dispatch(currentTimeUpdate()),
+    handlePreparedCheck: (idx: any) => dispatch(preparedCheck(idx)),
+    // onClick: (idx: any) => dispatch(preparedCheck(idx)),
   }),
 )(Home);
